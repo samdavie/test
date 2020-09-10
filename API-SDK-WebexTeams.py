18d852526d18624cf5d2560b91e4652bbfc41dea
Sam Davies <samdavie@cisco.com>
2020-09-10 11:04:52 +1000

    commit

---
+++ API-SDK-WebexTeams.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import urllib3
+urllib3.disable_warnings()
+import pprint
+import CREDENTIALS
+
+TOKEN = None
+
+def get_token():
+	url = ''
+	headers = {}
+	response = requests.post()
+	return None
+
+def list_room():
+	return None
+
+def create_room():
+	return None
+
+def list_messages():
+	return None
+
+def create_message():
+	return None
+
+def delete_message():
+	return None
+
+def list_people():
+	return None
+
+def get_my_details():
+	return None
+
+def get_room_details():
+	return None
+
+
+#make a change
+#another change
+#yet another change
+#what happens now
+
+if __name__ == '__main__':
+
+	TOKEN = get_token()
